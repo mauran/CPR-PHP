754984609102864b4cf0eaec54d95b9146b21a9d
Mauran Muthiah <Marwarndks@gmail.com>
2013-06-14 20:50:41 +0200

    Alex is killing me.

--- validate.php
+++ validate.php
@@ -2,15 +2,34 @@
 /*
 @Author: Mauran Muthiah
 @Description: :))
+
+[1, 3, 5, 7, 9]
 */
 
+class cpr {
+
+	function __construct($gender) {
+		$this->gender($gender);
+		
+	}
+
+	public function gender($gender)
+	{
+		if($gender == "male") {
+			return array('1','3','5','7','9');
+		}
+		elseif($gender == "female") {
+			return array('0','2','4','6','8');
+		} else {
+
+		}
+	}
 
 
 
+}
 
 
-
-
-
+$class = new cpr();
 
 ?>
