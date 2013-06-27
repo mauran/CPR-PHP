416e5f5550a94ef3c47dd3a9040eddaa98bac77d
Oliver Nordbjerg <nordbjerg.oliver@gmail.com>
2013-06-27 03:11:01 +0200

    Credit
    Please.

--- cpr.class.php
+++ cpr.class.php
@@ -2,9 +2,10 @@
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <mauran@mauran.me> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return --Mauran Muthiah
+ * <mauran@mauran.me> and <nordbjerg.oliver@gmail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with this
+ * stuff. If we meet some day, and you think this stuff is worth it, you can
+ * buy us a beer in return --Mauran Muthiah and Oliver Nordbjerg
  * ----------------------------------------------------------------------------
  */
 
